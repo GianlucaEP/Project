0bd2f305f7bf48bb693b37b011e4cea112592316
Marius <marius.nita@errepro.tech>
2020-07-27 10:22:54 +0200

    Creazione HomeController

--- ProgettoHR/src/it/rt/corso/controller/HomeController.java
+++ ProgettoHR/src/it/rt/corso/controller/HomeController.java
@@ -1,5 +1,14 @@
 package it.rt.corso.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
 
+	@RequestMapping("/Home")
+	public String display() {
+		return "Home";
+	}
+	
 }
