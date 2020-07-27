11c9ab86edcaac0de6b3b201839be821ff08b825
Marius <marius.nita@errepro.tech>
2020-07-27 11:46:39 +0200

    Creazione hibernate.cfg.xml Creazione Home.Controlle Creazione bean.xml

--- ProgettoHR/src/it/rt/corso/controller/HomeController.java
+++ ProgettoHR/src/it/rt/corso/controller/HomeController.java
@@ -1,14 +1,35 @@
 package it.rt.corso.controller;
 
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import it.rt.corso.DAO.CandidatoDAO;
+import it.rt.corso.beans.Candidato;
 
 @Controller
 public class HomeController {
 
+	ApplicationContext factory = new ClassPathXmlApplicationContext("bean.xml");
+
+	CandidatoDAO dao = (CandidatoDAO) factory.getBean("candidatoDAO");
+
 	@RequestMapping("/Home")
-	public String display() {
+	public String display(Model m) {
+		//List<Candidato> list = dao.getListaHome();
+		//m.addAttribute("list", list);
 		return "Home";
 	}
-	
+
+	@RequestMapping("/FormaCandidato")
+	public String showform(Model m) {
+		m.addAttribute("command", new Candidato());
+		return "empform";
+	}
+
+
 }
