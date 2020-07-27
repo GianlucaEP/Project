a66dc79c06b78544d8b3e24f3c7968db8c44c145
Marius <marius.nita@errepro.tech>
2020-07-27 17:05:58 +0200

    Correzione candidatiController

--- ProgettoHR/src/it/rt/corso/controller/CandidatiController.java
+++ ProgettoHR/src/it/rt/corso/controller/CandidatiController.java
@@ -1,10 +1,11 @@
 package it.rt.corso.controller;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@Controller
 public class CandidatiController {
 	@RequestMapping("/Candidati")
 	public String display(Model m) {
