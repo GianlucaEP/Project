6b5aaeca76fdc271989f986a6468b4fd7f21cd31
gheorghe.semco <gheorghe.semco@errepro.tech>
2020-07-29 17:18:08 +0200

    aggiunta annotazioni hibernate e configurazione mapping

--- ProgettoHR/src/it/rt/corso/beans/Colloquio.java
+++ ProgettoHR/src/it/rt/corso/beans/Colloquio.java
@@ -2,28 +2,43 @@ package it.rt.corso.beans;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "colloquio")
 public class Colloquio implements Bean{
 	
-	//attributi
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_colloquio")
 	private int id;
-	private int idCandidato; // id del candidato di riferimento
+//	@Column(name = "id_candidato")
+//	private int idCandidato; // TODO id del candidato di riferimento
+	@Column(name = "data")
 	private Date data;
+	@Column(name = "operatore")
 	private String operatore;
+	@Column(name = "commento")
 	private String commento;
 	
-	//Get & Set
+	
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
-	public int getIdCandidato() {
-		return idCandidato;
-	}
-	public void setIdCandidato(int idCandidato) {
-		this.idCandidato = idCandidato;
-	}
+//	public int getIdCandidato() {
+//		return idCandidato;
+//	}
+//	public void setIdCandidato(int idCandidato) {
+//		this.idCandidato = idCandidato;
+//	}
 	public Date getData() {
 		return data;
 	}
--- ProgettoHR/src/it/rt/corso/beans/Telefonata.java
+++ ProgettoHR/src/it/rt/corso/beans/Telefonata.java
@@ -2,28 +2,43 @@ package it.rt.corso.beans;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "telefonata")
 public class Telefonata implements Bean{
 	
-	//Attributi
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_telefonata")
 	private int id;
-	private int idCandidato; //id del candidato di riferimento
+//	@Column(name = "id_candidato")
+//	private int idCandidato; //TODO id del candidato di riferimento
+	@Column(name = "data")
 	private Date data;
+	@Column(name = "operatore")
 	private String operatore;
+	@Column(name = "commento")
 	private String commento;
 	
-	//Get & Set
+	
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
-	public int getIdCandidato() {
-		return idCandidato;
-	}
-	public void setIdCandidato(int idCandidato) {
-		this.idCandidato = idCandidato;
-	}
+//	public int getIdCandidato() {
+//		return idCandidato;
+//	}
+//	public void setIdCandidato(int idCandidato) {
+//		this.idCandidato = idCandidato;
+//	}
 	public Date getData() {
 		return data;
 	}
