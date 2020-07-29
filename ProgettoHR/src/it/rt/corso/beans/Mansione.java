77d5e5b32640640600b010077a8dd3d0a28be5e6
gheorghe.semco <gheorghe.semco@errepro.tech>
2020-07-29 15:26:39 +0200

    annotation classe mansione

--- ProgettoHR/src/it/rt/corso/beans/Mansione.java
+++ ProgettoHR/src/it/rt/corso/beans/Mansione.java
@@ -1,8 +1,21 @@
 package it.rt.corso.beans;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "mansione")
 public class Mansione implements Bean{
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_mansione")
 	private int id;
+	@Column(name = "mansione")
 	private String mansione;
 	
 	public int getId() {
