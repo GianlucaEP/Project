f8402e9571ba061d9cad536b8bf682e24827a2bf
Gianluca <gianluca.bova@errepro.tech>
2020-08-03 14:44:13 +0200

    commit LoginController

---
+++ ProgettoHR/src/it/rt/corso/controller/LoginController.java
@@ -0,0 +1,13 @@
+package it.rt.corso.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+	@RequestMapping("/Login")
+	public String display() {
+		return "Login";
+	}
+}
