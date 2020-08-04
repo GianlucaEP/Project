904a9efcbace87bca00fb9114e63f68ceff8c755
Gianluca <gianluca.bova@errepro.tech>
2020-08-04 11:22:05 +0200

    asd

--- ProgettoHR/src/it/rt/corso/controller/LoginController.java
+++ ProgettoHR/src/it/rt/corso/controller/LoginController.java
@@ -3,13 +3,14 @@ package it.rt.corso.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import it.rt.corso.beans.Utente;
 
 @Controller
 public class LoginController {
 
-	@RequestMapping("/Login")
+	@RequestMapping(value = "/Login", method = RequestMethod.POST)
 	public String display(@ModelAttribute Utente utente) {
 		
 		
