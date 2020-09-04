1690d586d26beb0634561215f64940dda5877cb2
stefano <stefano.schiavone@errepro.tech>
2020-09-04 10:43:02 +0200

    Aggiunta documentazione filtro stato

--- ProgettoHR/src/it/rt/corso/DAO/CandidatoDAO.java
+++ ProgettoHR/src/it/rt/corso/DAO/CandidatoDAO.java
@@ -27,6 +27,16 @@ public interface CandidatoDAO {
 	 * */
 	public List<Candidato> getListaByBusinessUnitFiltered(Map<String, String> mappaFilter);
 	
+	/** Create and execute query to Candidato table, joining Business unit specified by the String businessUnit, to get 
+	 * a List of type Candidato filtered by stato specified by the stato attribute.
+	 * 
+	 * @param businessUnit String that specifies the business unit wich the Candidato is part of
+	 * 
+	 * @param stato String that specifies the stato which is going to be used in the where clause.
+	 * 
+	 * @return the List of Candidato, wich has to be printed inside the table, filtered by the selected stato.
+	 * 
+	 * */
 	public List<Candidato> getListaByBusinessUnitFilteredByStato(String businessUnit, String stato);
 	
 	
