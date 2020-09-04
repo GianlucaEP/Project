462fd7bad2f9954dbc31ed71b6e8a2024311683b
stefano <stefano.schiavone@errepro.tech>
2020-09-04 10:22:06 +0200

    Aggiunta documentazione Bean

--- ProgettoHR/src/it/rt/corso/beans/Bean.java
+++ ProgettoHR/src/it/rt/corso/beans/Bean.java
@@ -1,5 +1,8 @@
 package it.rt.corso.beans;
-
+/** 
+ * Every object that will implements this interface will become of type Bean, 
+ * making them able to be used in method that use at lest a Bean parameter as method argument.
+ * */
 public interface Bean {
 
 }
