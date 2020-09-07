633ec32658732ea1997d0488e3157f5ba71009c2
stefano <stefano.schiavone@errepro.tech>
2020-09-07 16:33:21 +0200

    Rimosso metodo buildRootCriteriaForCandidato

--- ProgettoHR/src/it/rt/corso/utility/Utility.java
+++ ProgettoHR/src/it/rt/corso/utility/Utility.java
@@ -67,26 +67,5 @@ public class Utility {
 		session.close();
 	}
 
-	public static Root<Candidato> buildRootCriteriaForCandidato() {
-
-		Utility.buildSession();
-
-		// bean di candidatofilter nel argomento
-		// controllo i campi not null e ne faccio predicati
-
-		Session session = Utility.getSession();
-
-		// creo builder di criteria
-		CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-
-		// Crea a CriteriaQuery object with the specified resulttype. (classe candidato)
-		CriteriaQuery<Candidato> criteriaQuery = criteriaBuilder.createQuery(Candidato.class);
-
-		// Create and add a query root corresponding to the given entity,forming a
-		// cartesian product with any existing roots.
-		Root<Candidato> root = criteriaQuery.from(Candidato.class);
-
-		return root;
-	}
 	
 }
