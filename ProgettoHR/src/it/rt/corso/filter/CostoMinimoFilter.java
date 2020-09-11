5fe8f51449f54c03e6faef57ea435d73024a3a5a
stefano <stefano.schiavone@errepro.tech>
2020-09-11 11:53:59 +0200

    Filtro costi aggiustato

--- ProgettoHR/src/it/rt/corso/filter/CostoMinimoFilter.java
+++ ProgettoHR/src/it/rt/corso/filter/CostoMinimoFilter.java
@@ -3,12 +3,15 @@ package it.rt.corso.filter;
 import java.util.List;
 
 import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import org.hibernate.Session;
 
 import it.rt.corso.beans.Candidato;
+import it.rt.corso.beans.Costo;
 import it.rt.corso.utility.Utility;
 
 public class CostoMinimoFilter extends CandidatoFilter {
@@ -21,7 +24,9 @@ public class CostoMinimoFilter extends CandidatoFilter {
 			Session session = Utility.getSession();
 
 			CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-			listaPredicati.add(criteriaBuilder.greaterThan(root.get("costo"), Integer.parseInt(valore)));
+			Join<Candidato, Costo> costo = root.join("costo", JoinType.INNER);
+			
+			listaPredicati.add(criteriaBuilder.greaterThanOrEqualTo(costo.get("orario"), Integer.parseInt(valore)));
 			
 			CandidatoFilter.setAddedCriteria(true);
 
