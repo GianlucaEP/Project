e5de645f757d1724a1984518a403284905a06806
s.schiavone <s.schiavone@UTENTE051.erre.local>
2020-10-21 15:28:23 +0200

    Rimossi import non utilizzati

--- ProgettoHR/src/it/rt/corso/controller/UploadFileController.java
+++ ProgettoHR/src/it/rt/corso/controller/UploadFileController.java
@@ -1,38 +1,19 @@
 package it.rt.corso.controller;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import org.apache.commons.fileupload.FileItem;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
-
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import it.rt.corso.DAO.CandidatoDAO;
 import it.rt.corso.DAO.UploadFileDAO;
