c0be79d7cdfd2284bcdf6219c7f0a4add9460229
s.schiavone <s.schiavone@UTENTE051.erre.local>
2020-10-22 16:49:30 +0200

    Aggiunta creazione e download excel

--- ProgettoHR/src/it/rt/corso/controller/GanttController.java
+++ ProgettoHR/src/it/rt/corso/controller/GanttController.java
@@ -1,8 +1,11 @@
 package it.rt.corso.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -37,33 +40,13 @@ public class GanttController {
 		return "Gantt";
 	}
 
-	@SuppressWarnings("resource")
 	@RequestMapping("/ExcelGantt/{businessUnit}")
 	public String getExcel(Model m, @PathVariable String businessUnit, @SessionAttribute("utente") Utente utente,
-			HttpServletResponse response) throws IOException {
-		HSSFWorkbook wb = new HSSFWorkbook();
-		
-		try {
-			m.addAttribute("businessUnit", businessUnit);
+			HttpServletRequest request, HttpServletResponse response) throws IOException {
+	
+        CreateGanttExcel.downloadExcel(request, response);
 
-			
-
-			wb = CreateGanttExcel.createWorkbook(wb);
-			
-			String fileName = CreateGanttExcel.getFileName();
-
-			response.setContentType("application/vnd.ms-excel");
-			response.setHeader("Content-disposition", "attachment; filename="+fileName+".xlsx");
-			wb.write(response.getOutputStream());
-
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-		} finally {
-			if (wb != null) {
-				wb.close();
-			}
-		}
-		return "redirect:/Gantt/{businessUnit}";
+		return null;
 	}
 
 	
--- ProgettoHR/src/it/rt/corso/utility/CreateGanttExcel.java
+++ ProgettoHR/src/it/rt/corso/utility/CreateGanttExcel.java
@@ -1,53 +1,111 @@
 package it.rt.corso.utility;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class CreateGanttExcel {
-	
-	public static HSSFWorkbook createWorkbook(HSSFWorkbook wb) throws IOException{
-		
-		
-		HSSFSheet sheet = wb.createSheet();
-		
-		writeHeadersGantt(wb, sheet);
-		
-		return wb;
-		
+
+	public static XSSFWorkbook createWorkbook(XSSFWorkbook workbook) throws IOException {
+
+		XSSFSheet sheet = workbook.createSheet();
+
+		writeHeadersGantt(workbook, sheet);
+
+		return workbook;
+
 	}
-	
-	private static void writeHeadersGantt(HSSFWorkbook workbook, HSSFSheet sheet) {
-		HSSFRow header = sheet.createRow(0);
-        HSSFCell headerCell = header.createCell(0);
-        headerCell.setCellValue("Testing");
+
+	private static void writeHeadersGantt(XSSFWorkbook workbook, XSSFSheet sheet) {
+		XSSFRow header = sheet.createRow(0);
+		XSSFCell headerCell = header.createCell(0);
+		headerCell.setCellValue("Testing");
 	}
-	
+
 	/**
 	 * 
 	 * Create a fileName String appending the today date String to the file name.
 	 * 
 	 * @return the file name with today date
-	 *  
-	 *  */
+	 * 
+	 */
 	public static String getFileName() {
-		
+
 		String name = "Gantt";
-		
+
 		Date todayDate = new Date();
-		
+
 		SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy");
 		String date = DATE_FORMAT.format(todayDate);
-		
-		date.replaceAll("-", "");
-		
+
+		date = date.replaceAll("-", "");
+
 		return name.concat(date);
 	}
-	
+
+	@SuppressWarnings("resource")
+	public static void downloadExcel(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		File file = new File(System.getProperty("upload.location"), getFileName() + ".xlsx");
+
+		XSSFWorkbook workbook = new XSSFWorkbook();
+
+		workbook = CreateGanttExcel.createWorkbook(workbook);
+
+		FileOutputStream out = new FileOutputStream(file);
+		workbook.write(out);
+		downloadFile(file, response);
+		out.close();
+		workbook.close();
+
+	}
+
+	public static void downloadFile(File file, HttpServletResponse response) {
+		try {
+			response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+			response.addHeader("content-disposition", "attachment; filename="+getFileName() + ".xlsx");
+			response.setHeader("Pragma", "public");
+			response.setHeader("Cache-Control", "no-store");
+			response.addHeader("Cache-Control", "max-age=0");
+			FileInputStream fin = null;
+			try {
+				fin = new FileInputStream(file);
+			} catch (final FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			final int size = 1024;
+			try {
+				response.setContentLength(fin.available());
+				final byte[] buffer = new byte[size];
+				ServletOutputStream outputStream = null;
+
+				outputStream = response.getOutputStream();
+				int length = 0;
+				while ((length = fin.read(buffer)) != -1) {
+					outputStream.write(buffer, 0, length);
+				}
+				fin.close();
+				outputStream.flush();
+				outputStream.close();
+			} catch (final IOException e) {
+				e.printStackTrace();
+			}
+		} catch (final Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
 }
