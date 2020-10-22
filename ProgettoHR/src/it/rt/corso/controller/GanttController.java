6d8b5cff1c6acd9f6daf163f11051b623785042e
s.schiavone <s.schiavone@UTENTE051.erre.local>
2020-10-22 10:20:18 +0200

    Creazione Excel di test

--- ProgettoHR/src/it/rt/corso/controller/GanttController.java
+++ ProgettoHR/src/it/rt/corso/controller/GanttController.java
@@ -1,7 +1,11 @@
 package it.rt.corso.controller;
 
+import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
@@ -13,32 +17,55 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 import it.rt.corso.DAO.CandidatoDAO;
 import it.rt.corso.beans.Candidato;
 import it.rt.corso.beans.Utente;
+import it.rt.corso.utility.CreateGanttExcel;
 
 @Controller
 public class GanttController {
-	
+
 	private ApplicationContext factory = new ClassPathXmlApplicationContext("bean.xml");
 
 	private CandidatoDAO cdao = (CandidatoDAO) factory.getBean("candidatoDAO");
-	
-	@RequestMapping("/Gantt/{businessUnit}")
-	public String display(Model m,@PathVariable String businessUnit, @SessionAttribute("utente") Utente utente) {
-		
-		List<Candidato> list = cdao.getLista();
 
+	@RequestMapping("/Gantt/{businessUnit}")
+	public String display(Model m, @PathVariable String businessUnit, @SessionAttribute("utente") Utente utente) {
+
+		List<Candidato> list = cdao.getLista();
 
 		m.addAttribute("list", list);
 		m.addAttribute("businessUnit", businessUnit);
-		
+
 		return "Gantt";
 	}
-	
+
+	@SuppressWarnings("resource")
 	@RequestMapping("/ExcelGantt/{businessUnit}")
-	public String getExcel(Model m,@PathVariable String businessUnit, @SessionAttribute("utente") Utente utente) {
+	public String getExcel(Model m, @PathVariable String businessUnit, @SessionAttribute("utente") Utente utente,
+			HttpServletResponse response) throws IOException {
+		HSSFWorkbook wb = new HSSFWorkbook();
 		
-		m.addAttribute("businessUnit", businessUnit);
-		
+		try {
+			m.addAttribute("businessUnit", businessUnit);
+
+			
+
+			wb = CreateGanttExcel.createWorkbook(wb);
+			
+			String fileName = CreateGanttExcel.getFileName();
+
+			response.setContentType("application/vnd.ms-excel");
+			response.setHeader("Content-disposition", "attachment; filename="+fileName+".xlsx");
+			wb.write(response.getOutputStream());
+
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		} finally {
+			if (wb != null) {
+				wb.close();
+			}
+		}
 		return "redirect:/Gantt/{businessUnit}";
 	}
+
+	
 
 }
--- ProgettoHR/src/it/rt/corso/utility/CreateGanttExcel.java
+++ ProgettoHR/src/it/rt/corso/utility/CreateGanttExcel.java
@@ -1,4 +1,53 @@
 package it.rt.corso.utility;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+
 public class CreateGanttExcel {
+	
+	public static HSSFWorkbook createWorkbook(HSSFWorkbook wb) throws IOException{
+		
+		
+		HSSFSheet sheet = wb.createSheet();
+		
+		writeHeadersGantt(wb, sheet);
+		
+		return wb;
+		
+	}
+	
+	private static void writeHeadersGantt(HSSFWorkbook workbook, HSSFSheet sheet) {
+		HSSFRow header = sheet.createRow(0);
+        HSSFCell headerCell = header.createCell(0);
+        headerCell.setCellValue("Testing");
+	}
+	
+	/**
+	 * 
+	 * Create a fileName String appending the today date String to the file name.
+	 * 
+	 * @return the file name with today date
+	 *  
+	 *  */
+	public static String getFileName() {
+		
+		String name = "Gantt";
+		
+		Date todayDate = new Date();
+		
+		SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy");
+		String date = DATE_FORMAT.format(todayDate);
+		
+		date.replaceAll("-", "");
+		
+		return name.concat(date);
+	}
+	
 }
