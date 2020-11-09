e69d68248dec3ef858f4941e95c422b7f82e8e07
s.schiavone <s.schiavone@UTENTE051.erre.local>
2020-11-09 13:05:02 +0100

    Aggiunti colori alla lista

--- ProgettoHR/src/it/rt/corso/utility/CellColorList.java
+++ ProgettoHR/src/it/rt/corso/utility/CellColorList.java
@@ -8,7 +8,21 @@ public class CellColorList {
 	private static List<Short> cellColorList; 
 	
 	protected static List<Short> createCellColorList(){
+		cellColorList.add(IndexedColors.RED.getIndex());
+		cellColorList.add(IndexedColors.GREEN.getIndex());
+		cellColorList.add(IndexedColors.BLUE.getIndex());
+		cellColorList.add(IndexedColors.ORANGE.getIndex());
+		cellColorList.add(IndexedColors.YELLOW.getIndex());
 		cellColorList.add(IndexedColors.AQUA.getIndex());
+		cellColorList.add(IndexedColors.ROSE.getIndex());
+		cellColorList.add(IndexedColors.BRIGHT_GREEN.getIndex());
+		cellColorList.add(IndexedColors.LAVENDER.getIndex());
+		cellColorList.add(IndexedColors.RED1.getIndex());
+		cellColorList.add(IndexedColors.SEA_GREEN.getIndex());
+		cellColorList.add(IndexedColors.BLUE1.getIndex());
+		cellColorList.add(IndexedColors.BROWN.getIndex());
+		cellColorList.add(IndexedColors.BRIGHT_GREEN.getIndex());
+		cellColorList.add(IndexedColors.ROYAL_BLUE.getIndex());
 		
 		return cellColorList;
 	}
